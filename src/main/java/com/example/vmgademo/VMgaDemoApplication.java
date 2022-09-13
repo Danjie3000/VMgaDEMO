df08979bbaef4974df8c6a74022e4df1fe76c8f9
danjie3000 <dannie.kattrup@gmail.com>
2022-09-13 10:27:42 +0200

    første push, kun application.properties tilføjelse

---
+++ src/main/java/com/example/vmgademo/VMgaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.vmgademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VMgaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VMgaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vmgademo/VMgaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vmgademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VMgaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
